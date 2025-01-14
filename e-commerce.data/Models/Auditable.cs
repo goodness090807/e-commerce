0633c3c26295066a22f5b0e174a14abe46c4103b
蔡家誠(Terry) <37359587+goodness090807@users.noreply.github.com>
2025-01-14 22:10:42 +0800

    Delete e-commerce.data directory

--- e-commerce.data/Models/Auditable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace e_commerce.Data.Models
-{
-    public class Auditable : IAuditable
-    {
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-    }
-}
--- e-commerce.data/Models/IAuditable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace e_commerce.Data.Models
-{
-    public interface IAuditable
-    {
-        DateTime CreatedAt { get; set; }
-        DateTime UpdatedAt { get; set; }
-    }
-}
